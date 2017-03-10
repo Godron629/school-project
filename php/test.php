2498fd069ca4b55d5f4b99a779fdf648ab509d1e
Gideon <gideonrich@hotmail.ca>
2017-03-09 22:44:10 -0700

    Map changed form field to appropriate database column

--- php/test.php
+++ php/test.php
@@ -1,5 +1,7 @@
 <?php
-	var_dump($_GET);
+	$test = phpinfo();
+	var_dump($test);
+	$test = "hello";
 ?>
 
 <!DOCTYPE html>
--- php/updateVolunteer.php
+++ php/updateVolunteer.php
@@ -6,46 +6,74 @@
 
 function updateVolunteer() {
 	$changedFields = changedFields();
-	echo json_encode($changedFields);
+	$changedColumns = fieldNameToDatabaseColumn($changedFields);
 }
 
 function changedFields() {
-	$origForm = array();
-	$changeForm = array();
+	//Entire form was serialized with jQuery before Ajax call
+	$origForm = undoSerializeForm($_POST["form1"]);
+	$changeForm = undoSerializeForm($_POST["form2"]);
 
-	//Undo serialize done in jQuery before Ajax call
-	parse_str($_POST["form1"], $origForm);
-	parse_str($_POST["form2"], $changeForm);
+	$changedFields = valueOfChangedFields($origForm, $changeForm);
 
-	$fieldsThatChanged = keysOfChangedFields($origForm, $changeForm);
-
-	return $fieldsThatChanged;
+	return $changedFields;
 }
 
-function keysOfChangedFields($origForm, $changeForm) {
+function undoSerializeForm($form) {
+	$newForm = array();
+	parse_str($form, $newForm);
+	return $newForm;
+}
+
+function valueOfChangedFields($origForm, $changeForm) {
+	//Makes a boolean array of all the form fields. True value if field changed.
 	$didFieldChange = compareForms($origForm, $changeForm);
 
+	//Get the form input name attributes
 	$keysOfForm = array_keys($changeForm);
-	$keysOfChangedFields = array();
 
+	$changedFields = array();
+
+	//If the field changed, add the form input name attribute and the new value to the changedFields array
 	$i = 0;
 	foreach ($changeForm as $key => $value) {
 		if($didFieldChange[$i]) {
-			$keysOfChangedFields[] = $key;
+			$changedFields[$key] = $value;
 		}
 		$i++;
 	}
 
-	return $keysOfChangedFields;
+	return $changedFields;
 }
 
 function compareForms($origForm, $changeForm) {
-	//Compares two forms, returns boolean array. True is field is different across the forms.
+	//Compares two forms, returns boolean array. True is field value is different across the forms.
 	$didFieldChange = array_map(function($origForm, $changeForm) {
 			return $origForm != $changeForm;
 		}, $origForm, $changeForm);
 
 	return $didFieldChange;
+}
+
+function fieldNameToDatabaseColumn ($changedFields) {
+	//json file maps form input names to database columns
+	$jsonFile = file_get_contents($_SERVER["DOCUMENT_ROOT"] . '/javascript/databaseColumnNames.json');
+	$fieldMapToColumn = json_decode($jsonFile, true);
+
+	$changedColumns = array();
+
+	//Only does volunteer and emergency
+	foreach ($fieldMapToColumn as $key => $value) {
+		if(is_array($value)) {
+			foreach ($value as $key2 => $value2) {
+				if(in_array($key2, $changedFields)) {
+					$changedColumns[] = $value2;
+				}	
+			}
+		}
+	}
+
+	return $changedColumns;
 }
 		
 ?>
